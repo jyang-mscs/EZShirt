99562c9a6b52735a6d74da9db9929ef34be0cae1
Jefferson Yang <jeffersonyang01@gmail.com>
2025-09-15 20:23:53 -0500

    Update to Main Menu
    Added new panel for options pop up. In Main Menu script, add logic to hide menu buttons and show option pop up window. Added same background image to shirt creator scene.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,13 +1,19 @@
 extends Control
 
+@onready var main_menu_buttons = $MainMenuButtons
+@onready var option = $Option
+
 
 func _on_start_pressed():
-	print("start pressed")
+	get_tree().change_scene_to_file("res://EZShirt/scenes/shirt__creator.tscn")
 
 
 func _on_option_pressed():
 	print("option pressed")
+	main_menu_buttons.visible = false
+	option.visible = true
+	
 
 
 func _on_exit_pressed():
-	print("exit pressed")
+	get_tree().quit()
