ad4dae11153045729b5cf518c20264d3c05afc2c
Jefferson Yang <jeffersonyang01@gmail.com>
2025-09-15 20:11:25 -0500

    Update to Main Menu script
    Linked node signals to buttons. Added print statements to signals for testing. Testing performed well. Print output matches buttons pressed.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,1 +1,13 @@
 extends Control
+
+
+func _on_start_pressed():
+	print("start pressed")
+
+
+func _on_option_pressed():
+	print("option pressed")
+
+
+func _on_exit_pressed():
+	print("exit pressed")
